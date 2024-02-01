6dfe8ba4a7a4813f79ab2ad433325163c8c255e2
van khin <khmaister@yahoo.com>
2024-01-31 20:00:42 -0500

    functional generator

--- index.js
+++ index.js
@@ -1,13 +1,78 @@
-// TODO: Include packages needed for this application
+// Packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
+const generateMarkdown = require("./utils/generateMarkdown");
 
-// TODO: Create an array of questions for user input
-const questions = [];
+// User input prompts
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+  inquirer.prompt(
+    [
+      {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?',
+      },
 
-// TODO: Create a function to initialize app
-function init() {}
+      {
+        type: 'input',
+        name: 'description',
+        message: 'A brief description of your project:',
+      },
 
-// Function call to initialize app
-init();
+      {
+        type: 'input',
+        message: 'Describe the installation process:',
+        name: 'installation',
+      },
+
+      {
+        type: 'input',
+        message: 'What is the use of this project?',
+        name: 'usage',
+      },
+
+      {
+        type: 'input',
+        message: 'Who are the contributors?',
+        name: 'contributing',
+      },
+
+      {
+        type: 'input',
+        message: 'Are there any tests included?',
+        name: 'tests',
+      },
+
+      {
+        type: 'list',
+        message: 'Choose the license used in this project:',
+        name: 'license',
+        choices: [
+          'Apache', 'Academic', 'GNU', 'ISC', 'MIT', 'Mozilla', 'Open'
+        ]
+      },
+
+      {
+        type: "input",
+        name: "username",
+        message: "What is your GitHub username?",
+      },
+
+      {
+        type: "input",
+        name: "email",
+        message: "What is your email associated with Github?",
+      },
+
+    ]
+    )
+
+
+// Generate README file
+.then((answers) => {
+  const readMePage = generateMarkdown(answers);
+
+  fs.writeFile('./Example/README.md', readMePage, (err) =>
+  err ? console.log(err) : console.log('Successfully wrote to README.md')
+  )
+})
